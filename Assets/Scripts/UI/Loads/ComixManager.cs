f3ebdeb1a2cc16d293a29961a0939a6b32a07950
Ledozvezd <rekhtin.ilya@mail.ru>
2024-04-11 07:08:09 +0700

    LoadingAnimation

--- Assets/Scripts/UI/Loads/ComixManager.cs
+++ Assets/Scripts/UI/Loads/ComixManager.cs
@@ -6,20 +6,21 @@ using UnityEngine.SceneManagement;
 public class ComixManager : MonoBehaviour
 {
     [SerializeField] public Image[] images;
-    private int i = 0;
+    private int i = 1;
     private void Start()
     {
         DOTween.Init(true);
     }
     private void Update()
     {
+        images[0].DOColor(new Color(1, 1, 1, 1.0f), 1.5f);
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
         {
             images[i].DOColor(new Color(1, 1, 1, 1.0f), 1.5f);
             i++;
             if (i > images.Length - 1)
             {
-                SceneManager.LoadScene("Level");
+                LoadManager.SwitchToScene("Level");
             }
         }
     }
